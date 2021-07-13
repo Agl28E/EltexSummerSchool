0512d75a870e328236c90de1aea69037ab6fcdf5
Agl28E <85914453+Agl28E@users.noreply.github.com>
2021-07-13 21:01:27 +0700

    Update task1.c

--- fork/task1.c
+++ fork/task1.c
@@ -8,22 +8,24 @@ int main()
 {
   pid_t pid, ppid;
   int rv = 0;
-  switch(pid = fork()) {
-  case -1:
+  pid = fork(); 
+  if (pid == -1){
           perror("fork = -1"); /* произошла ошибка */
           exit(1); /*выход из родительского процесса*/
-  case 0:
+  } else {
+      if (pid == 0){
           printf(" CHILD: Это процесс-потомок!\n");
           printf(" CHILD: Мой PID -- %d\n", getpid());
-          printf(" CHILD: PID моего родителя -- %d\n",getppid());
+          printf(" CHILD: PID моего родителя -- %d\n", getppid());
           exit(rv);
-  default:
+     
+  } else {
           printf("PARENT: Это процесс-родитель!\n");
           printf("PARENT: Мой PID -- %d\n", getpid());
-          printf("PARENT: PID моего потомка %d\n",pid);
+          printf("PARENT: PID моего потомка %d\n", pid);
           printf("PARENT: Я жду, пока потомок не вызовет exit()...\n");
           wait(NULL);
   }
-  return 0;
+  }
+return 0;
 }
-
