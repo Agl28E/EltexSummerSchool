c47ab212047ee04f62538b416121e27985d72a8f
Agl28E <85914453+Agl28E@users.noreply.github.com>
2021-07-13 19:41:05 +0700

    Create task1.c

---
+++ fork/task1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main()
+{
+  pid_t pid, ppid;
+  int rv = 0;
+  switch(pid = fork()) {
+  case -1:
+          perror("fork = -1"); /* произошла ошибка */
+          exit(1); /*выход из родительского процесса*/
+  case 0:
+          printf(" CHILD: Это процесс-потомок!\n");
+          printf(" CHILD: Мой PID -- %d\n", getpid());
+          printf(" CHILD: PID моего родителя -- %d\n",getppid());
+          exit(rv);
+  default:
+          printf("PARENT: Это процесс-родитель!\n");
+          printf("PARENT: Мой PID -- %d\n", getpid());
+          printf("PARENT: PID моего потомка %d\n",pid);
+          printf("PARENT: Я жду, пока потомок не вызовет exit()...\n");
+          wait(NULL);
+  }
+  return 0;
+}
+
